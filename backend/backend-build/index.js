d8ecd0b01b27b1529e5ffc2a75b369c865e844e4
Matthew Francis <mattfrancis888@gmail.com>
2021-03-12 14:52:13 -0500

    Configured to run on heroku.

--- backend/backend-build/index.js
+++ backend/backend-build/index.js
@@ -8,6 +8,7 @@ var dotenv_1 = __importDefault(require("dotenv"));
 var cors_1 = __importDefault(require("cors"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var path_1 = __importDefault(require("path"));
 if (process.env.NODE_ENV !== "production") {
     dotenv_1.default.config();
 }
@@ -32,6 +33,22 @@ app.use("/api/test", function (req, res) {
 app.use("/api", routes_1.default);
 app.use("/api", media_1.default);
 // app.use("/", protectedRouter);
+//NOTE: app.use below that uses authenticateToken MUST ALWAYS BE LAST of the routes.
+// At every "/" , if we have an expired JWT, it will send an error
+//and none of the routes afterwards will work. By putting it last, we could send an error and get a new access token with "/token"
+//WE WILL NOT BE USING THIS BELOW BECAUSE WE ARE NOT SENDING AUTHORIZATION HEADERS
+//BECAUSE NOCK CANNOT MOCK HTTP REQUESTS WITH HEADERS
+//app.use("/api", authenticateToken);
+//All other roues will be handled by
+app.use(express_1.default.static(path_1.default.join(__dirname, "build")));
+app.get("*", function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
+});
+// const root = require("path").join(__dirname, "build");
+// app.use(express.static(root));
+// app.get("*", (req, res) => {
+//     res.sendFile("index.html", { root });
+// });
 var port = 5000;
 app.listen(port, function () {
     console.log("App running on port " + port + ".");
